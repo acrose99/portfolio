13b0e65f7bf0b5aad34b796c2f81479e6198519a
Alexander Rose <acrbrazil99@gmail.com>
2022-02-02 18:02:59 -0600

    fix bug with dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,28 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 function MyApp({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false);
   const [themeType, setThemeType] = useState("light");
   const switchThemes = () => {
     setThemeType((last) => (last === "dark" ? "light" : "dark"));
   };
   useEffect(() => {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
       setThemeType("dark");
     }
   }, []);
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
   return (
     <GeistProvider themeType={themeType}>
       <CssBaseline />
       <Navbar switchThemes={switchThemes} />
-      <Component
-      switchThemes={switchThemes}
-      {...pageProps} />
+      <Component switchThemes={switchThemes} {...pageProps} />
       <Footer />
     </GeistProvider>
   );
