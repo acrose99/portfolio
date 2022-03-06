0a101c70f12e59f7d52afa933b867fdf5e3b8835
Alexander Rose <acrbrazil99@gmail.com>
2022-03-06 10:32:15 -0600

    add next-sep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,8 @@ import Footer from "../components/Footer";
 import Script from "next/script";
 import useSound from "use-sound";
 import Head from "next/head";
+import { DefaultSeo } from "next-seo";
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = useState(false);
   const [themeType, setThemeType] = useState("light");
@@ -34,27 +36,98 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   if (!mounted) return null;
   return (
-    <>
-      <GeistProvider themeType={themeType}>
-        <CssBaseline />
-        <Navbar switchThemes={switchThemes} />
-        <Component switchThemes={switchThemes} {...pageProps} />
-        <Footer />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-04KLC3DPQS"
-          strategy="afterInteractive"
+    <GeistProvider themeType={themeType}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="language" content="english" />
+        <meta httpEquiv="content-type" content="text/html" />
+        <meta name="author" content="Alex Rose" />
+        <meta name="designer" content="Alex Rose" />
+        <meta name="publisher" content="Alex Rose" />
+        <meta
+          name="google-site-verification"
+          content="VdvpZmr1DbaSdgpZajFCWx4patEfFHA6o1gH42zSFv0"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          color="#000000"
+          href="/icons/safari-pinned-tab.svg"
+        />
+        <link rel="apple-touch-startup-image" href="/startup.png" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+
+        {/* Meta Tags for HTML pages on Mobile */}
+        {/* <meta name="format-detection" content="telephone=yes"/>
+        <meta name="HandheldFriendly" content="true"/>  */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1"
+        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <DefaultSeo 
+        title="Alex Rose"
+        description="Alex Rose is a Software Developer located in Chicago"
+        canonical="https://www.alexrose.xyz/"
+        openGraph={{
+          url: 'https://www.alexrose.xyz/',
+          title: 'Alex Rose',
+          description: 'Alex Rose is a Software Developer located in Chicago',
+          images: [
+            {
+              url: 'https://alexrose.xyz/ogImage1.png',
+              width: 180,
+              height: 110,
+              alt: 'Alex Rose',
+              type: 'image/png',
+            },
+            {
+              url: 'https://alexrose.xyz/ogImage2.png',
+              width: 1200,
+              height: 630,
+              alt: 'Alex Rose',
+              type: 'image/png',
+            },
+          ],
+          site_name: 'Alex Rose',
+        }}
+      />
+      <CssBaseline />
+      <Navbar switchThemes={switchThemes} />
+      <Component switchThemes={switchThemes} {...pageProps} />
+      <Footer />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-04KLC3DPQS"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-04KLC3DPQS');
         `}
-        </Script>
-      </GeistProvider>
-    </>
+      </Script>
+    </GeistProvider>
   );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,8 @@ import NavigationInput from "../components/NavigationInput";
 import Project from "../components/Projects/Project";
 import Music from "../components/Music";
 import { getSongs } from "./api/getSongs";
-import useSound from "use-sound";
 import Slide from "../components/Hero/Slide";
-import Image from "next/image";
+import { NextSeo } from "next-seo";
 
 type song = {
   artist: string;
@@ -233,76 +232,6 @@ export default function Home({ songs }: HomeProps) {
   }
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="language" content="english" />
-        <meta httpEquiv="content-type" content="text/html" />
-        <meta name="author" content="Alex Rose" />
-        <meta name="designer" content="Alex Rose" />
-        <meta name="publisher" content="Alex Rose" />
-        <meta
-          name="google-site-verification"
-          content="VdvpZmr1DbaSdgpZajFCWx4patEfFHA6o1gH42zSFv0"
-        />
-        {/* Search Engine Optimization Meta Tags */}
-        <title>Alex Rose</title>
-        <meta
-          name="description"
-          content="Alex Rose is a Software Developer located in Chicago"
-        />
-        <meta
-          name="keywords"
-          content="Alex Rose, Alex Rose Loyola, Alex Rose Developer, Alex Rose Chicago, Alex Digital Humanities"
-        />
-        <meta name="robots" content="index,follow" />
-        <meta name="distribution" content="web" />
-        {/*
-Facebook Open Graph meta tags
-documentation: https://developers.facebook.com/docs/sharing/opengraph */}
-        <meta property="og:type" content="site" />
-        <meta property="og:url" content="https://www.alexrose.xyz/" />
-        <meta property="og:site_name" content="Alex Rose" />
-        <meta property="og:title" content="Alex Rose's Site" />
-        <meta
-          property="og:description"
-          content="Alex Rose is a Software Engineer located in Chicago"
-        />
-        <meta property="og:image" content="https://alexrose.xyz/ogImage.png" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="mask-icon"
-          color="#000000"
-          href="/icons/safari-pinned-tab.svg"
-        />
-        <link rel="apple-touch-startup-image" href="/startup.png" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-
-        {/* Meta Tags for HTML pages on Mobile */}
-        {/* <meta name="format-detection" content="telephone=yes"/>
-<meta name="HandheldFriendly" content="true"/>  */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1"
-        />
-        <link rel="shortcut icon" href="/icons/favicon.ico" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="space-y-8 min-h-screen">
         <div className="text-center">
           <Text h1>Alex Rose.</Text>
